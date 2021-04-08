cdb9f533a75d20628b207928140aa330573e783b
Петров Евгений Сергеевич <petrov@tochka.com>
2021-04-08 14:57:37 +0500

    Add ESLint

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Lazy Component
+
+## Config
+
+**components/Loading.component.tsx**
+
+```tsx
+export const LoadingComponent = (): JSX.Element =>
+  <div>Loading...</div>;
+```
+
+**components/Error.component.tsx**
+
+```tsx
+export const ErrorComponent = ({ error }: { error: Error }): JSX.Element =>
+  <div>
+    <h1>{error.name}</h1>
+    <p>{error.message}</p>
+  </div>
+```
+
+**utils/lazyPage.tsx**
+
+```tsx
+import { LoadingComponent, ErrorComponent } from 'src/components';
+import { createLazyPage } from 'lazy-page';
+
+export const lazyPage = createLazyPage({
+  LoadingComponent,
+  ErrorComponent
+});
+```
+
+## Usage
+
+**App.tsx**
+
+```tsx
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { lazyPage } from './utils';
+
+export const App = (): JSX.Element => <BrowserRouter>
+  <Switch>
+    <Route path={'/'} component={lazyPage(() => import('/pages/MainPage'))} exact/>
+    <Route path={'/about'} component={lazyPage(() => import('/pages/AboutPage'))} exact/>
+  </Switch>
+</BrowserRouter>;
+```
--- index.tsx
+++ index.tsx
@@ -2,56 +2,59 @@ import { ComponentType, memo, useEffect, useState } from 'react';
 import { LocationDescriptor, Location, LocationState } from 'history';
 import { Redirect, useLocation, useParams } from 'react-router';
 
+type TProps = Record<string, unknown>;
+type TParams = Record<string, string>;
+
 interface LazyPageConfig {
-    LoadingComponent: ComponentType;
-    ErrorComponent: ComponentType<{ error: Error }>;
+  LoadingComponent: ComponentType;
+  ErrorComponent: ComponentType<{ error: Error }>;
 }
 
-export type GetInitPropsContext<Params extends object, State = LocationState> = { params: Params } & Location<State>;
-export type GetInitPropsResult<Props extends object> = { props: Props } | { redirect: LocationDescriptor };
-export type GetInitialProps<Props extends object = object, Params extends object = object> =
-    <State>(ctx: GetInitPropsContext<Params, State>) => Promise<GetInitPropsResult<Props>>;
+export type GetInitPropsContext<Params extends TParams, State = LocationState> = { params: Params } & Location<State>;
+export type GetInitPropsResult<Props extends TProps> = { props: Props } | { redirect: LocationDescriptor };
+export type GetInitialProps<Props extends TProps = TProps, Params extends TParams = TParams> =
+  <State>(ctx: GetInitPropsContext<Params, State>) => Promise<GetInitPropsResult<Props>>;
 
-export interface LazyPage<Props extends object, Params extends object = object> {
-    default: ComponentType<Props>;
-    getInitialProps?: GetInitialProps<Props, Params>;
+export interface LazyPage<Props extends TProps, Params extends TParams = TParams> {
+  default: ComponentType<Props>;
+  getInitialProps?: GetInitialProps<Props, Params>;
 }
 
 enum LazyPageState {
-    LOADING,
-    SUCCESS,
-    ERROR
+  LOADING,
+  SUCCESS,
+  ERROR
 }
 
-type State<Props extends object> =
-    | { state: LazyPageState.LOADING }
-    | { state: LazyPageState.ERROR; error: Error }
-    | { state: LazyPageState.SUCCESS; Component: ComponentType<Props> } & GetInitPropsResult<Props>;
+type State<Props extends TProps> =
+  | { state: LazyPageState.LOADING }
+  | { state: LazyPageState.ERROR; error: Error }
+  | { state: LazyPageState.SUCCESS; Component: ComponentType<Props> } & GetInitPropsResult<Props>;
 
 export const createLazyPage = (config: LazyPageConfig) =>
-    <Props extends object, Params extends object = object>(factory: () => Promise<LazyPage<Props, Params>>) =>
-        memo(() => {
-            const [state, setDynamicState] = useState<State<Props>>({ state: LazyPageState.LOADING });
-            const params = useParams<Params>();
-            const location = useLocation<unknown>();
+  <Props extends TProps, Params extends TParams = TParams>(factory: () => Promise<LazyPage<Props, Params>>): ComponentType =>
+    memo(() => {
+      const [state, setDynamicState] = useState<State<Props>>({ state: LazyPageState.LOADING });
+      const params = useParams<Params>();
+      const location = useLocation<unknown>();
 
-            useEffect(() => {
-                (async function loading(): Promise<void> {
-                    try {
-                        const { default: Component, getInitialProps } = await factory();
-                        const props = getInitialProps
-                            ? await getInitialProps({ ...location, params })
-                            : { props: {} } as GetInitPropsResult<Props>;
+      useEffect(() => {
+        (async function loading(): Promise<void> {
+          try {
+            const { default: Component, getInitialProps } = await factory();
+            const props = getInitialProps
+              ? await getInitialProps({ ...location, params })
+              : { props: {} } as GetInitPropsResult<Props>;
 
-                        setDynamicState({ state: LazyPageState.SUCCESS, Component, ...props });
-                    } catch (error) {
-                        setDynamicState({ state: LazyPageState.ERROR, error });
-                    }
-                }());
-            }, [params, location]);
+            setDynamicState({ state: LazyPageState.SUCCESS, Component, ...props });
+          } catch (error) {
+            setDynamicState({ state: LazyPageState.ERROR, error });
+          }
+        }());
+      }, [params, location]);
 
-            if (state.state === LazyPageState.LOADING) return <config.LoadingComponent/>;
-            if (state.state === LazyPageState.ERROR) return <config.ErrorComponent error={state.error}/>;
-            if ('redirect' in state) return <Redirect to={state.redirect}/>;
-            return <state.Component {...state.props}/>;
-        });
+      if (state.state === LazyPageState.LOADING) return <config.LoadingComponent/>;
+      if (state.state === LazyPageState.ERROR) return <config.ErrorComponent error={state.error}/>;
+      if ('redirect' in state) return <Redirect to={state.redirect}/>;
+      return <state.Component {...state.props}/>;
+    });
